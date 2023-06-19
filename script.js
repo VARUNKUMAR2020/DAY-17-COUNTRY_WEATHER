5c6d8d4dd3199ac101eeaef9c60d29c4efe237d6
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2023-06-19 17:41:40 +0530

    Add:Weather-response

--- script.js
+++ script.js
@@ -3,19 +3,64 @@ fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((country) => {
-      // console.log(country);
-      const countryCard = document.createElement("a");
+      const countryCard = document.createElement("div");
       countryCard.classList.add("country-card");
-      countryCard.href = `/country.html?name=${country.name.common}`;
-      countryCard.innerHTML = `
-            <h3 class="card-title">${country.name.common}</h3>
-            <img src="${country.flags.svg}" alt="flag" />
-            <div class="card-text">
-            <p><b>Capital: </b>${country.capital?.[0]}</p>
-            <p><b>Region: </b>${country.region}</p>
-            <p><b>Country Code: </b>${country.cca3}</p>
-            <button onclick="weather()"type="button" class="btn btn-primary">Click for weather</button>
-             </div>`;
-      countriesContainer.append(countryCard);
+      const countryName = country.name.common;
+
+      const countryTitle = document.createElement("h3");
+      countryTitle.classList.add("card-title");
+      countryTitle.textContent = countryName;
+      countryCard.appendChild(countryTitle);
+
+      const flagImage = document.createElement("img");
+      flagImage.src = country.flags.svg;
+      flagImage.alt = "flag";
+      countryCard.appendChild(flagImage);
+
+      const countryInfo = document.createElement("div");
+      countryInfo.classList.add("card-text");
+      countryInfo.innerHTML = `
+        <p><b>Capital: </b>${country.capital?.[0]}</p>
+        <p><b>Region: </b>${country.region}</p>
+        <p><b>Country Code: </b>${country.cca3}</p>
+      `;
+      countryCard.appendChild(countryInfo);
+
+      const weatherButton = document.createElement("button");
+      weatherButton.type = "button";
+      weatherButton.classList.add("btn", "btn-primary");
+      weatherButton.textContent = "Click for weather";
+      countryCard.appendChild(weatherButton);
+
+      const temperatureDisplay = document.createElement("p");
+      temperatureDisplay.classList.add("temperature-display");
+      countryCard.appendChild(temperatureDisplay);
+
+      countriesContainer.appendChild(countryCard);
+
+      weatherButton.addEventListener("click", () => getWeather(countryName, temperatureDisplay));
     });
   });
+
+function getWeather(countryName, temperatureDisplay) {
+  fetch(`https://restcountries.com/v2/name/${countryName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const latitude = data[0].latlng[0];
+      const longitude = data[0].latlng[1];
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=ae0ddc20aca0de9b50d6e35eac27a537`
+      )
+        .then((response) => response.json())
+        .then((weatherData) => {
+          const temperature = weatherData.main.temp;
+          temperatureDisplay.textContent = `Temperature: ${temperature}°C`;
+        })
+        .catch((error) => {
+          console.log("Error retrieving weather data:", error);
+        });
+    })
+    .catch((error) => {
+      console.log("Error retrieving country data:", error);
+    });
+}
